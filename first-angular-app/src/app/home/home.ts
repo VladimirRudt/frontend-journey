89b349ef6b4d9f931dd993b18276f906b4e8b410
Vladimir Rudt <vladimir.rudt@saritasa.com>
2026-01-25 21:31:25 +0700

    Add housing list

---
+++ first-angular-app/src/app/home/home.ts
@@ -0,0 +1,32 @@
+import { Component, inject } from '@angular/core';
+import { HousingLocation } from "../housing-location/housing-location";
+import { HousingLocationInfo } from '../housinglocation';
+import { Housing } from '../housing';
+
+@Component({
+  selector: 'app-home',
+  imports: [HousingLocation],
+  template: `
+    <section>
+      <form>
+        <input type="text" placeholder="Filter by city" />
+        <button class="primary" type="button">Search</button>
+      </form>
+    </section>
+    <section class="results">
+      @for (housingLocation of housingLocationList; track $index) {
+        <app-housing-location [housingLocation]="housingLocation" />
+      }
+    </section>
+  `,
+  styleUrl: "./home.css"
+})
+export class Home {
+  housingService: Housing = inject(Housing)
+
+  housingLocationList: HousingLocationInfo[] = []
+
+  constructor() {
+    this.housingLocationList = this.housingService.getAllHousingLocations();
+  }
+}
