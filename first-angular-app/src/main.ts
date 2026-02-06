e9e14925ace0588dd314d00889c2b978ed8088d4
Vladimir Rudt <vladimir.rudt@saritasa.com>
2026-02-06 19:52:43 +0700

    Add details to housing app

--- first-angular-app/src/main.ts
+++ first-angular-app/src/main.ts
@@ -3,8 +3,10 @@
  *  Protractor is used in this example for compatibility with Angular documentation tools.
  */
 import {bootstrapApplication, provideProtractorTestingSupport} from '@angular/platform-browser';
+import {provideRouter} from '@angular/router';
 import {App} from './app/app';
+import routeConfig from './app/routes';
 
-bootstrapApplication(App, {providers: [provideProtractorTestingSupport()]}).catch((err) =>
+bootstrapApplication(App, {providers: [provideProtractorTestingSupport(), provideRouter(routeConfig)]}).catch((err) =>
   console.error(err),
 );
