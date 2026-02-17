791f207a49be92592260aa0bc087337cde5fd7c3
Kulakova Viktoria <tmw.kva@yandex.ru>
2026-02-17 22:47:34 +0700

    Initialize card component

--- blog/src/app/shared/components/link-list/link-list.ts
+++ blog/src/app/shared/components/link-list/link-list.ts
@@ -1,10 +1,11 @@
 import { Component, input } from '@angular/core';
 import { LinkListData } from './LinkListData';
 import { RouterLink } from '@angular/router';
+import { Card } from "../../card/card";
 
 @Component({
   selector: 'app-link-list',
-  imports: [RouterLink],
+  imports: [RouterLink, Card],
   templateUrl: './link-list.html',
   styleUrl: './link-list.scss',
 })
